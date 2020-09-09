d10181646d2991ca977f9fc39b2167e16758cd53
Finn <finnterdal@gmail.com>
2020-09-09 15:22:18 -0500

    adds some styles

--- client/index.js
+++ client/index.js
@@ -9,13 +9,23 @@ const App = () => {
       <h1 style={{ textAlign: 'center' }}>Hello Pokemon</h1>
       <h2 style={{ textAlign: 'center' }}>Let's Get Sorted</h2>
       <button>Sort!</button>
-      <div>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {pokemonIds.map((id) => {
           return (
-            <img
+            <div
               key={id}
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
-            />
+              style={{
+                border: '1px solid black',
+                width: '120px',
+                height: '120px',
+                margin: '10px',
+              }}
+            >
+              <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+              />
+              <div style={{ textAlign: 'center' }}>#{id}</div>
+            </div>
           );
         })}
       </div>
